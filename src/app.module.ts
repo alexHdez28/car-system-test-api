1eec604da1f1f59828ca5b0381e2effab5340ee2
alejandro <alexhernandez281092@gmail.com>
2021-02-09 22:55:39 -0600

    Enable cors, use of env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VehicleModule} from './vehicles/vehicle.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [VehicleModule, MongooseModule.forRoot(
+  imports: [
+    VehicleModule, 
+    MongooseModule.forRoot(
     'mongodb+srv://car-api:tCkcLaV8sqa6dNr7@cluster0.v4avq.mongodb.net/car-system-db?retryWrites=true&w=majority'
-  )],
+    ),
+    ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
